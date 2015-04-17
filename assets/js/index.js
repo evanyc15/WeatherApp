ed2ce5ea7fd34a09a7f394bdf8f8a99a6d088ff5
Evan Chen <echen@192.168.1.21>
2015-04-16 19:19:41 -0700

    Minor

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,18 +14,23 @@ $(document).ready(function(){
         dataType: 'json',
         success: function(response){
             var data = response.query.results.channel;
-            var condition = data.item.condition.text;
+            var condition = data.item.condition;
             var date = new Date(data.item.condition.date);
+            var code = parseInt(condition.code);
 
             // Checks the forecast weather condition and sets appropriate background image
-            if(condition.toLowerCase() === "fair"){
+            if(condition.text.toLowerCase() === "fair" || code === 33 || code === 34 || code === 31){
                 $("#fairWeatherBackgroundImage").show();
-            } else if(condition.toLowerCase() === "sunny"){
+            } else if(condition.text.toLowerCase() === "sunny" || code === 36 || code === 32){
                 $("#sunnyWeatherBackgroundImage").show();
-            } else if(condition.toLowerCase() === "cloudy"){
+            } else if(condition.text.toLowerCase() === "cloudy" || (code >= 26 && code <= 30) || code === 44){
                 $("#cloudyWeatherBackgroundImage").show();
-            } else if(condition.toLowerCase() === "rainy"){
+            } else if(condition.text.toLowerCase() === "rainy" || (code >= 8 && code <= 12) || code === 40){
                 $("#rainyWeatherBackgroundImage").show();
+            } else if(condition.text.toLowerCase() === "snowy" || (code >= 13 && code <= 16) || (code >= 41 && code <= 43) || code === 46){
+                $("#snowyWeatherBackgroundImage").show();
+            } else if(condition.text.toLowerCase() === "thunderstorm" || code === 3 || code === 4 || (code >= 37 && condition.code <= 39) || condition.code === 45 || condition.code === 47){
+                $("#thunderstormWeatherBackgroundImage").show();
             }
 
             // Populate Today's forecast information
@@ -37,6 +42,8 @@ $(document).ready(function(){
             $("#weatherLocation").text(data.location.city+", "+data.location.region)
             $("#weatherCond").text(data.item.condition.text);
 
+            console.log(data);
+
             // Populate the forecast for the week
             var forecast = data.item.forecast;
             var id;
@@ -45,7 +52,6 @@ $(document).ready(function(){
                 id = "#nextforecast" + counter;
                 var container = $(id);
 
-                console.log(forecast[i].text);
                 // Set the icon to the weather condition respectively
                 if(parseInt(forecast[i].date.substring(0,2)) ===  date.getDate()){
                     container.addClass("today");
@@ -71,9 +77,25 @@ $(document).ready(function(){
             }
             // Show page now
             setTimeout(function(){
-                $("#pageLoadingContainer").hide();
-                $("#container").css("visibility","visible");
+                //$("#pageLoadingContainer").hide();
+                //$("#container").css("visibility","visible");
             }, 3100);
+        }
+    });
+    $(document).mouseup(function (e) {
+        var container = $("#quickPanelContainer");
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if ($(e.target).hasClass("fa-gears") || $(e.target).attr("id") === "quickPanelButton") {
+            if ($("#quickPanelButton").hasClass("open") && $("#quickPanelContainer").hasClass("open")) {
+                container.removeClass("open");
+                $("#quickPanelButton").removeClass("open");
+            } else {
+                $("#quickPanelButton").addClass("open");
+                $("#quickPanelContainer").addClass("open");
+            }
+        } else if ((!container.is(e.target) && container.has(e.target).length === 0)) {
+            container.removeClass("open");
         }
     });
 });
